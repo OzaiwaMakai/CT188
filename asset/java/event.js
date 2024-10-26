45cddde63ce21158400b1482440b1b8c155538c9
DangKhoi1 <khoiplbl@gmail.com>
2024-10-26 23:28:13 +0700

    update new content java

--- asset/java/event.js
+++ asset/java/event.js
@@ -1,12 +1,23 @@
 // Đăng Ký và đăng nhập
 
+function dangnhap(){
+    alert("Đăng nhập thành công!");
+}
+
+
 function formValidate() {
     var email = document.getElementById("Email");
     var req = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-
+    var matkhau = document.getElementById("taomatkhau");
+    var req1 = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/g;
     // Kiểm tra email hợp lệ
     if (req.test(email.value) == false) {
         alert("Vui lòng nhập email hợp lệ");
+        return false;
+    }
+    
+    if(req1.test(matkhau.value)==false){
+        alert("Mật khẩu tối thiểu 8 ký tự, ít nhất 1 chữ cái viết hoa, 1 chữ cái viết thường, 1 số và 1 ký tự đặc biệt");
         return false;
     }
     return true;
@@ -25,8 +36,13 @@ function testpass(){
 function dangky() {
     if (formValidate() && testpass()) {
         alert("Đăng ký thành công!");
+        chuyenhuong();
     }
+    return true;
 }
 
+function chuyenhuong(){
+    window.location.href = 'dangnhap.html';
+}
 
 // Đăng Ký và đăng nhập
